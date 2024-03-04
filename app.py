af49e26eb0f462ee813c4ddd1b5bbc546d196ad1
jarvis2324 <gauravbarua614@gmail.com>
2024-03-04 12:14:10 +0530

    Updated System Prompt

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ index = load_data()
 
 if "chat_engine" not in st.session_state.keys(): # Initialize the chat engine
         # st.session_state.chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
-        st.session_state.chat_engine = index.as_chat_engine(chat_mode="context", memory=memory, verbose=True, system_prompt="Please provide an answer based solely on the provided sources.If none of the sources are helpful, you should indicate that by telling please call the risk management representative on call.")
+        st.session_state.chat_engine = index.as_chat_engine(chat_mode="context", memory=memory, verbose=False, system_prompt="You are going to answer questions regarding Hospital Policies. You can only make a conversation if the answer is from the provided sources if none of the sources are relevant to the question, respond politely \"Please call the Risk Management Representative on call.\" ")
 
 if prompt := st.chat_input("Your question"): # Prompt for user input and save to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
